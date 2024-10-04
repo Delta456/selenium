233e1339bd1d3c9c18dd9ccf93a1c0b2f40745c8
delta456 <swstkbaranwal@gmail.com>
2024-10-04 17:53:12 +0530

    add electron service

--- py/selenium/webdriver/electron/service.py
+++ py/selenium/webdriver/electron/service.py
@@ -17,3 +17,29 @@
 import typing
 
 from selenium.webdriver.chromium import service
+
+
+class Service(service.ChromiumService):
+    """A Service class that is responsible for the starting and stopping of
+    `chromiumdriver` as Electron is based upon Chromium.
+
+    :param executable_path: install path of the msedgedriver executable, defaults to `msedgedriver`.
+    :param service_args: (Optional) List of args to be passed to the subprocess when launching the executable.
+    :param env: (Optional) Mapping of environment variables for the new process, defaults to `os.environ`.
+    """
+
+    def __init__(
+        self,
+        executable_path: str = None,
+        service_args: typing.Optional[typing.List[str]] = None,
+        env: typing.Optional[typing.Mapping[str, str]] = None,
+        **kwargs,
+    ) -> None:
+        self.service_args = service_args or []
+
+        super().__init__(
+            executable_path=executable_path,
+            service_args=service_args,
+            env=env,
+            **kwargs,
+        )
